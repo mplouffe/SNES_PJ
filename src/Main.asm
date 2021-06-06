9c79f3e224c58fc59a02ee488b9a44a2879d36b5
mplouffe <matheuplouffe@gmail.com>
2021-06-05 22:52:35 -0500

    Added and loaded default palette.

--- src/Main.asm
+++ src/Main.asm
@@ -5,6 +5,7 @@
 .include "macros.asm"
 .include "init.asm"
 
+
 .segment "CODE"
 
 ; enters here // in foced blank
@@ -14,20 +15,48 @@ main:
   phk   ; setst he data bank register to the same as the Program Bank
   plb
 
+; DMA from BG_Palette to CGRAM
   A8              ; macro to put registers in 8 bit mode
+  stz pal_addr    ; $2121 cg address = zero
+
+  stz $4300 ; transfer mode 0 = 1 register write once
+  lda #$22 ; $2122
+  sta $4301 ; destination palette data
   
-  stz pal_addr    ;set color address to 0
-  lda #$1f        ;palette low byte   gggrrrrr
-  sta pal_data    ;1f = all the red bits
-  lda #$00        ;palette high byte  -bbbbbgg
-  sta pal_data    ;store zero for high byte
+  ldx #.loword(BG_Palette)
+  stx $4302 ; source
+  lda #^BG_Palette
+  sta $4304 ; bank
+  ldx #256  ; BG_Palette only has 128 colors
+  stx $4305 ; length
+  lda #1
+  sta $420b ; start dma, channel 0
+
+
+; do it again with the same DMA settings
+; you will have to reset the source and length
+; though the palette address will auto-increment on each write
+  ldx #.loword(BG_Palette)
+  stx $4302 ; source
+  lda #^BG_Palette
+  sta $4304 ; bank
+  ldx #256  ; BG_Palette only has 128 colors
+  stx $4305 ; length
+  lda #1
+  sta $420b ; start dma, channel 0
+
+
 
 ; turn screen on // end of forced blank
-
-  lda #$0f
-  sta $2100
+  lda #FULL_BRIGHT ; $0f = turn the screen on, full brightness
+  sta fb_bright ; $2100
 
 InfiniteLoop:
   jmp InfiniteLoop
 
 .include "header.asm"
+
+.segment "RODATA1"
+
+BG_Palette:
+.incbin "default.pal"
