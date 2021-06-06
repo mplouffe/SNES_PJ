30f79b09eb67d11f488161980dcfa0c75afc51ed
mplouffe <matheuplouffe@gmail.com>
2021-06-06 01:35:56 -0500

    Added custom Chewie BG and rle encoding.

--- src/Main.asm
+++ src/Main.asm
@@ -4,6 +4,7 @@
 .include "define.asm"
 .include "macros.asm"
 .include "init.asm"
+.include "unrle.asm"
 
 
 .segment "CODE"
@@ -20,30 +21,84 @@ main:
   stz pal_addr    ; $2121 cg address = zero
 
   stz $4300 ; transfer mode 0 = 1 register write once
-  lda #$22 ; $2122
+  lda #$22  ; $2122
   sta $4301 ; destination palette data
   
   ldx #.loword(BG_Palette)
   stx $4302 ; source
   lda #^BG_Palette
   sta $4304 ; bank
-  ldx #256  ; BG_Palette only has 128 colors
+  ldx #64
   stx $4305 ; length
   lda #1
   sta $420b ; start dma, channel 0
 
 
-; do it again with the same DMA settings
-; you will have to reset the source and length
-; though the palette address will auto-increment on each write
-  ldx #.loword(BG_Palette)
-  stx $4302 ; source
-  lda #^BG_Palette
-  sta $4304 ; bank
-  ldx #256  ; BG_Palette only has 128 colors
-  stx $4305 ; length
+; DMA from Tiles to VRAM
+  lda #V_INC_1  ; the value $80
+  sta vram_inc  ; $2115 = set the increment mode +1
+  ldx #$0000
+  stx vram_addr ; set an address in the vram of $0000
+
   lda #1
-  sta $420b ; start dma, channel 0
+  sta $4300   ; transfer mode, 2 registers 1 write
+              ; $2118 and $2119 are a pair Low/High
+  lda #$18    ; $2118
+  sta $4301   ; destination, vram data
+
+; - decompress first
+  AXY16
+  lda #.loword(Tiles)
+  ldx #^Tiles
+  jsl unrle   ; unpacks to 7f0000 UNPACK_ADR
+  ; returns y = length
+  ; ax = unpack address (x is bank)
+  sta $4302   ; source
+  txa
+  A8
+  sta $4304   ; bank
+  sty $4305   ; length
+  lda #1
+  sta $420b   ; start dma, channel 0
+
+
+; DMA from Tilemap to VRAM
+  ldx #$6000
+  stx vram_addr ; set an address in the vram of $6000
+
+; - decompress first
+  AXY16
+  lda #.loword(Tilemap)
+  ldx #^Tilemap
+  jsl unrle     ; unpacks to 7f0000 UNPACK_ADR
+  ; returns y = length
+  ; ax = unpack addr (x is bank)
+  sta $4302     ; source
+  txa
+  A8
+  sta $4304     ; bank
+  sty $4305     ; length
+  lda #1
+  sta $420b     ; start dma, channel 0
+
+
+; a is still 8 bit
+  lda #1
+  sta bg_size_mode  ; $2105
+
+; 210b = tilesets for bg 1 and bg 2
+; (210c for bg 3 and bg 4)
+; setps of $1000 -321-321... bg2 bg1
+  stz bg12_tiles    ; #210b BG 1 and 2 TILES at address $0000
+
+  ; 2107 map address bg1, steps of $400, but -54321yx
+  ; y/x = map size... 0,0 = 32x32 tiles
+  ; $6000 / $100 = $60
+  lda #$60        ; address $6000
+  sta tilemap1    ; $2107
+
+  lda #BG1_0N     ; only bg1 is active
+  sta main_screen ; #212c
 
 
 
@@ -59,4 +114,12 @@ InfiniteLoop:
 .segment "RODATA1"
 
 BG_Palette:
-.incbin "default.pal"
+.incbin "chewie.pal"
+
+Tiles:
+; 4bpp tileset compressed
+.incbin "RLE/tileset.rle"
+
+Tilemap:
+; tilemap compressed
+.incbin "RLE/map.rle"
